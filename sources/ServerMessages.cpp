774eab56620d96224ebdcfd8f25c3c89866e21a9
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-12-04 17:37:18 +0000

    added more message functions

--- sources/ServerMessages.cpp
+++ sources/ServerMessages.cpp
@@ -12,9 +12,62 @@ ServerMessages&	ServerMessages::operator = ( const ServerMessages& assign ) {
 	return ( *this );
 }
 
+void	ServerMessages::ERR_NOSUCHCHANNEL( const int socketFd, const std::string& clientName, const std::string& channelName ) {
+	std::string	message( "Tijolinhos 475 " + clientName + " " + channelName + " :No such channel\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
 
-void		ServerMessages::sendError475( const int socketFd, const std::string& channelName, const std::string& clientName ) {
+void	ServerMessages::ERR_NONICKNAMEGIVEN( const int socketFd, const std::string& clientName ) {
+	std::string	message( "Tijolinhos 475 " + clientName  + " :No nickname given\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
+
+void	ServerMessages::ERR_NICKNAMEINUSE( const int socketFd, const std::string& clientName, const std::string& nick ) {
+	std::string	message( "Tijolinhos 475 " + clientName + " " + nick + " :Nickname is already in use\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
+
+void	ServerMessages::ERR_USERNOTINCHANNEL( const int socketFd, const std::string& clientName, const std::string& nick, const std::string& channelName ) {
+	std::string	message( "Tijolinhos 475 " + clientName + " " + nick + " " + channelName + " :They aren't on that channel\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
+
+void	ServerMessages::ERR_NOTONCHANNEL( const int socketFd, const std::string& clientName, const std::string& channelName ) {
+	std::string	message( "Tijolinhos 475 " + clientName + " " + channelName + " :You're not on that channel\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
+
+void	ServerMessages::ERR_NEEDMOREPARAMS( const int socketFd, const std::string& clientName, const std::string& command ) {
+	std::string	message( "Tijolinhos 475 " + clientName + " " + command + " :Not enough parameters\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
+
+void	ServerMessages::ERR_ALREADYREGISTERED( const int socketFd, const std::string& clientName ) {
+	std::string	message( "Tijolinhos 475 " + clientName + " :You may not reregister\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
+
+void	ServerMessages::ERR_PASSWDMISMATCH( const int socketFd, const std::string& clientName ) {
+	std::string	message( "Tijolinhos 475 " + clientName + " :Password incorrect\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
+
+void	ServerMessages::ERR_BADCHANNELKEY( const int socketFd, const std::string& clientName, const std::string& channelName ) {
 	std::string	message( "Tijolinhos 475 " + clientName + " " + channelName + " :Cannot join channel (+k)\r\n" );
+	
+	send( socketFd, message.c_str(), message.size(), 0 );
+}
+
+void	ServerMessages::ERR_CHANOPRIVSNEEDED( const int socketFd, const std::string& clientName, const std::string& channelName ) {
+	std::string	message( "Tijolinhos 475 " + clientName + " " + channelName + " :You're not channel operator\r\n" );
 	
 	send( socketFd, message.c_str(), message.size(), 0 );
 }

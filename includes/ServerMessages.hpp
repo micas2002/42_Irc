4431efb6ccb07e1c0981bf6edbc0f78655f38f7d
CanIBeFii <filipe250502@gmail.com>
2023-12-06 16:24:55 +0000

    style: comments with name and number of errors

--- includes/ServerMessages.hpp
+++ includes/ServerMessages.hpp
@@ -10,20 +10,30 @@ class ServerMessages {
 		// REPLIES
 		// RPL_ENDOFWHO 315
 		static void		RPL_ENDOFWHO( int socketFd, const std::string& clientName, const std::string& mask );
-
 		// RPL_WHOREPLY 352
 		static void		RPL_WHOREPLY( int socketFd, User* user,  const std::string& sender, const std::string& target );
 
-		static void			ERR_NOSUCHCHANNEL( const int socketFd, const std::string& clientName, const std::string& channelName );
-		static void			ERR_NONICKNAMEGIVEN( const int socketFd, const std::string& clientName );
-		static void			ERR_NICKNAMEINUSE( const int socketFd, const std::string& clientName, const std::string& nick );
-		static void			ERR_USERNOTINCHANNEL( const int socketFd, const std::string& clientName, const std::string& nick, const std::string& channelName );
-		static void			ERR_NOTONCHANNEL( const int socketFd, const std::string& clientName, const std::string& channelName );
-		static void			ERR_NEEDMOREPARAMS( const int socketFd, const std::string& clientName, const std::string& command );
-		static void			ERR_ALREADYREGISTERED( const int socketFd, const std::string& clientName );
-		static void			ERR_PASSWDMISMATCH( const int socketFd, const std::string& clientName	);
-		static void			ERR_BADCHANNELKEY( const int socketFd, const std::string& clientName, const std::string& channelName );
-		static void			ERR_CHANOPRIVSNEEDED( const int socketFd, const std::string& clientName, const std::string& channelName );
+		// ERROR
+		// ERR_NOSUCHCHANNEL 403
+		static void		ERR_NOSUCHCHANNEL( const int socketFd, const std::string& clientName, const std::string& channelName );
+		// ERR_NONICKNAMEGIVEN 431
+		static void		ERR_NONICKNAMEGIVEN( const int socketFd, const std::string& clientName );
+		// ERR_NICKNAMEINUSE 433
+		static void		ERR_NICKNAMEINUSE( const int socketFd, const std::string& clientName, const std::string& nick );
+		// ERR_USERNOTINCHANNEL 441
+		static void		ERR_USERNOTINCHANNEL( const int socketFd, const std::string& clientName, const std::string& nick, const std::string& channelName );
+		// ERR_NOTONCHANNEL 442
+		static void		ERR_NOTONCHANNEL( const int socketFd, const std::string& clientName, const std::string& channelName );
+		// ERR_NEEDMOREPARAMS 461
+		static void		ERR_NEEDMOREPARAMS( const int socketFd, const std::string& clientName, const std::string& command );
+		// ERR_ALREADYREGISTERED 462
+		static void		ERR_ALREADYREGISTERED( const int socketFd, const std::string& clientName );
+		// ERR_PASSWDMISMATCH 464
+		static void		ERR_PASSWDMISMATCH( const int socketFd, const std::string& clientName	);
+		// ERR_BADCHANNELKEY 475
+		static void		ERR_BADCHANNELKEY( const int socketFd, const std::string& clientName, const std::string& channelName );
+		// ERR_CHANOPRIVSNEEDED 482
+		static void		ERR_CHANOPRIVSNEEDED( const int socketFd, const std::string& clientName, const std::string& channelName );
 
 	private:
 		ServerMessages();
